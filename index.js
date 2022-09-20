38ad935485d8690e26ccafef5ee00342dbc7dafc
Jelani Cannon <jelani.cannon@gmail.com>
2022-09-20 12:27:47 -0700

    commit tree 7a6594065120eab43e93439160b77bab34908a75

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+
+// function random(min, max){
+//     min = Math.ceil(min)
+//     max = Math.floor(max)
+//     return Math.floor(Math.random() * (max-min)+1) +min;
+   
+// }
+const random = Math.floor(Math.random() * Math.random())+1
+
+const num3 = 16 
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(5,10,15,20)
+
+
